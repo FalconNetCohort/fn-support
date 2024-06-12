a407d471f2073d1b106c2741528d2c1e23932752
ark-232 <arkady232@gmail.com>
2024-06-12 17:20:11 -0600

    chore: Add cn utility function for merging class values
    -- added flowing link preview

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,19 +1,27 @@
 "use client";
-import Link from "next/link";
+import { LinkPreview } from "../components/ui/link-preview";
 
 export default function Footer() {
-  return (
-    <footer className="w-full p-4 bg-gray-800 text-white flex flex-col items-center">
-      <div className="flex flex-col items-start">
-        <h2 className="text-lg font-bold">Helpful Links</h2>
-        <Link href="https://github.com/FalconNetCohort" className="mt-2">GitHub Organization</Link>
-        <Link href="https://falconnet.us" className="mt-2">FalconNet</Link>
-        <Link href="https://apps.apple.com/us/app/falconnet/id6445867869" className="mt-2">Apple App Store</Link>
-        <Link href="https://play.google.com/store/apps/details?id=com.arkwerk.falconnet&pcampaignid=web_share" className="mt-2">Google Play Store</Link>
-      </div>
-      <div className="mt-4">
-        <p>&copy; {new Date().getFullYear()} FalconSupport. All rights reserved.</p>
-      </div>
-    </footer>
-  );
+    return (
+        <footer className="w-full p-4 bg-gray-800 text-white flex flex-col items-center">
+            <div className="flex flex-col items-start">
+                <h2 className="text-lg font-bold">Helpful Links</h2>
+                <LinkPreview url="https://github.com/FalconNetCohort" className="mt-2">
+                    GitHub Organization
+                </LinkPreview>
+                <LinkPreview url="https://falconnet.us" className="mt-2">
+                    FalconNet
+                </LinkPreview>
+                <LinkPreview url="https://apps.apple.com/us/app/falconnet/id6445867869" className="mt-2">
+                    Apple App Store
+                </LinkPreview>
+                <LinkPreview url="https://play.google.com/store/apps/details?id=com.arkwerk.falconnet&pcampaignid=web_share" className="mt-2">
+                    Google Play Store
+                </LinkPreview>
+            </div>
+            <div className="mt-4">
+                <p>&copy; {new Date().getFullYear()} FalconSupport. All rights reserved.</p>
+            </div>
+        </footer>
+    );
 }
