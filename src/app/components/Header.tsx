bc39247b949391333c2f194d12b2f8e57c1b0032
tomisnhamilton <tomisnhamilton@gmail.com>
2024-09-28 14:23:31 -0600

    Update Header.tsx

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -6,7 +6,6 @@ export default function Header() {
         <header className="w-full p-4 bg-blue-600 text-white flex justify-between items-center">
             <h1 className="text-2xl font-bold"><Link href="/" className="mx-2">Falcon Support</Link></h1>
             <nav>
-                <Link href="/in-progress" className="mx-2">In Progress</Link>
                 <Link href="/feature-request" className="mx-2">Feature Request</Link>
                 <Link href="/support-bug-report" className="mx-2">Support/Bug Report</Link>
                 <Link href="/dashboard" className="mx-2">Admin Dashboard</Link>
