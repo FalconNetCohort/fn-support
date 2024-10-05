11f2d7afa25368cf330540d4aea7f838ff85e814
Timothy Zhuang <C28Timothy.Zhuang@afacademy.af.edu>
2024-10-05 10:15:19 -0600

    setup themeing

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,26 +1,39 @@
 "use client";
+import { useTheme } from "next-themes";
 import { LinkPreview } from "../components/ui/link-preview";
+import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
 
 export default function Footer() {
+    const {theme, setTheme} = useTheme();
     return (
-        <footer className="w-full p-4 bg-gray-800 text-white flex flex-col items-center">
-            <div className="flex space-x-2">
-                <LinkPreview url="https://github.com/FalconNetCohort" className="mt-2">
+        <footer className="w-full p-4 bg-gray-800 text-white flex items-center pt-5">
+            <div className="flex-1">
+                <p>&copy; {new Date().getFullYear()} FalconSupport. All rights reserved.</p>
+            </div>
+            <div className="flex space-x-3">
+                <LinkPreview url="https://github.com/FalconNetCohort" className="text-gray-300">
                     GitHub Organization
                 </LinkPreview>
-                <LinkPreview url="https://falconnet.us" className="mt-2">
+                <LinkPreview url="https://falconnet.us" className="text-gray-300">
                     FalconNet
                 </LinkPreview>
-                <LinkPreview url="https://apps.apple.com/us/app/falconnet/id6445867869" className="mt-2">
+                <LinkPreview url="https://apps.apple.com/us/app/falconnet/id6445867869" className="text-gray-300">
                     Apple App Store
                 </LinkPreview>
-                <LinkPreview url="https://play.google.com/store/apps/details?id=com.arkwerk.falconnet&pcampaignid=web_share" className="mt-2">
+                <LinkPreview url="https://play.google.com/store/apps/details?id=com.arkwerk.falconnet&pcampaignid=web_share" className="text-gray-300">
                     Google Play Store
                 </LinkPreview>
             </div>
-            <div className="mt-4">
-                <p>&copy; {new Date().getFullYear()} FalconSupport. All rights reserved.</p>
-            </div>
+            {theme === "light" ? (
+                <button onClick={() => setTheme("dark")}>
+                    <MoonIcon className="w-5 h-5 ml-5"/>
+                </button>
+            ) : (
+                <button onClick={() => setTheme("light")}>
+                    <SunIcon className="w-5 h-5 ml-5"/>
+                </button>
+            )}
+
         </footer>
     );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import React from "react";
+import { ThemeProvider } from "next-themes";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +18,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ThemeProvider>{children}</ThemeProvider></body>
     </html>
   );
 }
