ac3c4ab9d795560c8a35d95714f423f18cc0e8ee
Grant <stecgrant89@gmail.com>
2024-10-05 09:47:41 -0600

    ui change

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -4,19 +4,21 @@ import { usePathname } from "next/navigation";
 
 export default function Header() {
     const tabs = ["Home", "Feature Request", "Support/Bug Report", "In Progress"];
-    const adminTabs = ["Admin Dashboard", "User Guide Admin"]
-    const pathname = usePathname()
+    const adminTabs = ["Admin Dashboard", "User Guide Admin"];
+    const pathname = usePathname();
 
     return (
         <header className="w-full p-4 bg-blue-600 text-white flex justify-between items-center">
-            <h1 className="text-2xl font-bold">
-                <Link href="/" className="mx-2">Falcon Support</Link></h1>
-            <nav className="text-gray-300">     
+            <Link href="/" className="mx-2">
+                <img src="falconsupport.png" alt="Falcon Support" className="h-8 w-auto" />
+            </Link>
+            <nav className="text-gray-300">
                 {tabs.map((tab) => (
-                    <Link 
-                    href={tab==="Home"?"/":`/${tab.toLowerCase().replace(/[\s\/]/g, "-")}`} 
-                    key={tab} 
-                    className={`mx-2 ${pathname === (tab==="Home"?"/":`/${tab.toLowerCase().replace(/[\s\/]/g, "-")}`) && "text-white"}`}>
+                    <Link
+                        href={tab === "Home" ? "/" : `/${tab.toLowerCase().replace(/[\s\/]/g, "-")}`}
+                        key={tab}
+                        className={`mx-2 ${pathname === (tab === "Home" ? "/" : `/${tab.toLowerCase().replace(/[\s\/]/g, "-")}`) && "text-white"}`}
+                    >
                         {tab}
                     </Link>
                 ))}
